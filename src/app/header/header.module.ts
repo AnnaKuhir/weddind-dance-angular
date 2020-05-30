b18fdd616e329c0e96080d88e28ab6ebd32d83db
Anna <anna.kuhir@nure.ua>
2020-05-30 14:25:43 +0300

    added header module

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavigationComponent,
+    HeaderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
